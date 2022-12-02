3be29d9c79022e1100fdc38173bc24634a225dcb
Jérémie Bonal <jeremie.bonal@gmail.com>
2022-12-02 15:02:35 +0100

    Remove example data in Day 01 code

--- day-01.rb
+++ day-01.rb
@@ -2,21 +2,15 @@ require_relative 'common'
 
 class Day1 < AdventDay
   def first_part
-    input.last(2).sum
   end
 
   def second_part
-    input.last(2).map(&:to_s).map(&:reverse).map(&:to_i).sum
   end
 
   private
 
   def convert_data(data)
-    super.map(&:to_i)
-  end
-
-  def debug_input
-    "12\n34\n56"
+    super
   end
 end
 
